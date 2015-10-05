6bbe3aa02816624126924960f33b4a0880feba93
Alex Banks <alex@moonhighway.com>
2015-10-05 09:52:30 -0700

    Adds Bower and Require js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-var app = new AppRouter();
+require(["/js/SkiAreaModel.js",
+         "/js/SkiAreaCollection.js",
+         "/js/SkiAreaView.js",
+         "/js/SkiAreasView.js",
+         "/js/ManageMyAreasView.js",
+         "/js/AppRouter.js"], function (SkiAreaModel, SkiAreaCollection, SkiAreaView, SkiAreasView, ManageMyAreasView, AppRouter) {
+
+    window.app = new AppRouter();
+
+});
+
+
+
